22a18eb824eb36c9008b481891fa5154193c88da
David Haywood <hazemamewip@hotmail.com>
2026-05-04 21:24:10 +0100

    gpl95xx progress

--- src/mame/tvgames/generalplus_gpl951xx.cpp
+++ src/mame/tvgames/generalplus_gpl951xx.cpp
@@ -11,16 +11,24 @@
 */
 
 #include "emu.h"
-#include "generalplus_gpl16250.h"
 
+#include "screen.h"
+#include "speaker.h"
+
+#include "machine/generalplus_gpl951xx_soc.h"
+#include "machine/generic_spi_flash.h"
 
 namespace {
 
-class generalplus_gpspi_direct_game_state : public gcm394_game_state
+class generalplus_gpspi_direct_game_state : public driver_device
 {
 public:
 	generalplus_gpspi_direct_game_state(const machine_config& mconfig, device_type type, const char* tag) :
-		gcm394_game_state(mconfig, type, tag)
+		driver_device(mconfig, type, tag),
+		m_maincpu(*this, "maincpu"),
+		m_screen(*this, "screen"),
+		m_genspi(*this, "spi"),
+		m_io(*this, "IN%u", 0U)
 	{
 	}
 
@@ -32,22 +40,60 @@ protected:
 	virtual void machine_start() override ATTR_COLD;
 	virtual void machine_reset() override ATTR_COLD;
 
-	virtual uint16_t cs0_r(offs_t offset) override;
+	uint16_t porta_r();
+	uint16_t portb_r();
+	uint16_t portc_r();
+	void porta_w(uint16_t data);
+
+	required_device<generalplus_gpl951xx_device> m_maincpu;
+	required_device<screen_device> m_screen;
+	required_device<generic_spi_flash_device> m_genspi;
+	required_ioport_array<3> m_io;
 };
+
+
+uint16_t generalplus_gpspi_direct_game_state::porta_r()
+{
+	uint16_t data = m_io[0]->read();
+	logerror("Port A Read: %04x\n", data);
+	return data;
+}
+
+uint16_t generalplus_gpspi_direct_game_state::portb_r()
+{
+	uint16_t data = m_io[1]->read();
+	logerror("Port B Read: %04x\n", data);
+	return data;
+}
+
+uint16_t generalplus_gpspi_direct_game_state::portc_r()
+{
+	uint16_t data = m_io[2]->read();
+	logerror("Port C Read: %04x\n", data);
+	return data;
+}
+
+void generalplus_gpspi_direct_game_state::porta_w(uint16_t data)
+{
+	logerror("%s: Port A:WRITE %04x\n", machine().describe_context(), data);
+}
 
 void generalplus_gpspi_direct_game_state::machine_start()
 {
-	gcm394_game_state::machine_start();
+	m_genspi->set_rom_ptr(memregion("spi")->base());
+	m_genspi->set_rom_size(memregion("spi")->bytes());
+	m_maincpu->set_spi_romregion(memregion("spi")->base(), memregion("spi")->bytes());
 }
 
 void generalplus_gpspi_direct_game_state::machine_reset()
 {
-	cs_callback(0x00, 0x00, 0x00, 0x00, 0x00);
-
 	m_maincpu->reset(); // reset CPU so vector gets read etc.
 }
 
 static INPUT_PORTS_START( bfmpac )
+	PORT_START("IN0")
+	PORT_START("IN1")
+
 	PORT_START("IN2")
 	PORT_DIPNAME( 0x0001, 0x0000, "0" )
 	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
@@ -85,9 +131,6 @@ static INPUT_PORTS_START( bfmpac )
 	PORT_DIPNAME( 0x8000, 0x0000, DEF_STR( Unknown ) )
 	PORT_DIPSETTING(      0x0000, DEF_STR( Off ) )
 	PORT_DIPSETTING(      0x8000, DEF_STR( On ) )
-
-	PORT_START("IN1")
-	PORT_START("IN0")
 INPUT_PORTS_END
 
 static INPUT_PORTS_START( bfspyhnt )
@@ -97,29 +140,17 @@ static INPUT_PORTS_START( bfspyhnt )
 	PORT_BIT( 0x4000, IP_ACTIVE_HIGH, IPT_BUTTON2 )
 INPUT_PORTS_END
 
-
-uint16_t generalplus_gpspi_direct_game_state::cs0_r(offs_t offset)
-{
-	// TODO: is cs_space even used by this type?
-	return 0x00;
-}
-
 void generalplus_gpspi_direct_game_state::generalplus_gpspi_direct(machine_config &config)
 {
-	set_addrmap(0, &generalplus_gpspi_direct_game_state::cs_map_base);
-
 	GPL951XX(config, m_maincpu, 96000000/2, m_screen);
 	m_maincpu->porta_in().set(FUNC(generalplus_gpspi_direct_game_state::porta_r));
 	m_maincpu->portb_in().set(FUNC(generalplus_gpspi_direct_game_state::portb_r));
 	m_maincpu->portc_in().set(FUNC(generalplus_gpspi_direct_game_state::portc_r));
 	m_maincpu->porta_out().set(FUNC(generalplus_gpspi_direct_game_state::porta_w));
-	m_maincpu->space_read_callback().set(FUNC(generalplus_gpspi_direct_game_state::read_external_space));
-	m_maincpu->space_write_callback().set(FUNC(generalplus_gpspi_direct_game_state::write_external_space));
 	m_maincpu->set_irq_acknowledge_callback(m_maincpu, FUNC(sunplus_gcm394_base_device::irq_vector_cb));
 	m_maincpu->add_route(ALL_OUTPUTS, "speaker", 0.5, 0);
 	m_maincpu->add_route(ALL_OUTPUTS, "speaker", 0.5, 1);
 	m_maincpu->set_bootmode(0);
-	m_maincpu->set_cs_config_callback(FUNC(gcm394_game_state::cs_callback));
 	m_maincpu->set_cs_space(DEVICE_SELF, 0);
 
 	SCREEN(config, m_screen, SCREEN_TYPE_RASTER);
@@ -130,18 +161,18 @@ void generalplus_gpspi_direct_game_state::generalplus_gpspi_direct(machine_confi
 	m_screen->set_screen_update("maincpu", FUNC(sunplus_gcm394_device::screen_update));
 	m_screen->screen_vblank().set(m_maincpu, FUNC(sunplus_gcm394_device::vblank));
 
+	GENERIC_SPI_FLASH(config, m_genspi, 0);
+
 	SPEAKER(config, "speaker", 2).front();
 }
 
-// Is there an internal ROM that gets mapped out or can this type really execute directly from scrambled SPI?
-// there doesn't appear to be anywhere to map an internal ROM, nor access to it, so I think they just execute
-// from SPI
+// There should be a small internal ROM (0x4000) bytes that does some basic setup
 
 ROM_START( fixitflx )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x200000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "fixitfelix_md25q16csig_c84015.bin", 0x0000, 0x200000, CRC(605c6863) SHA1(4f6cc2e8388e20eb90c6b05265273650eeea56eb) )
 ROM_END
 
@@ -149,7 +180,7 @@ ROM_START( wiwcs )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "wwcs.img", 0x0000, 0x800000, CRC(9b86bc45) SHA1(17721c662642a257d3e0f56e351a9a80d75d9110) )
 
 	ROM_REGION16_BE(0x400, "i2c", ROMREGION_ERASE00)
@@ -160,7 +191,7 @@ ROM_START( bfpacman )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x100000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "basicfunpacman_25q80_c84014.bin", 0x0000, 0x100000, CRC(dd39fc64) SHA1(48c0e1eb729f61b7359e1fd52b7faab56817dfe8) )
 ROM_END
 
@@ -168,7 +199,7 @@ ROM_START( bfmpac )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x100000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "mspacman_25q80_c84014.bin", 0x0000, 0x100000, CRC(c0c3f8ce) SHA1(30da9b14f1a2c966167c97da9b8329f2f7f73291) )
 ROM_END
 
@@ -176,7 +207,7 @@ ROM_START( bfdigdug )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x100000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "arcadeclassicsdigdug_25q80csig_c84014.bin", 0x0000, 0x100000, CRC(4030bc46) SHA1(8c086c96b9822e95c1862012786d6d6e59e0387e) )
 ROM_END
 
@@ -184,7 +215,7 @@ ROM_START( bfgalaga )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x100000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "arcadeclassicsgalaga_25q80csig_c84014.bin", 0x0000, 0x100000, CRC(69982c9d) SHA1(0f8f403fefa7d8a9fdfcc04dca5a67919b662c7e) )
 ROM_END
 
@@ -192,7 +223,7 @@ ROM_START( bfspyhnt )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x200000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "arcadeclassicsspyhunter_md25q16csig_c84015.bin", 0x0000, 0x200000, CRC(1f1eaabd) SHA1(1c484e0b0749123cfa1ac6d1959aefa6ed09ab20) )
 
 	// also has a 24C04 (to store high scores?)
@@ -202,146 +233,146 @@ ROM_START( bftetris )
 	ROM_REGION16_BE( 0x40000, "maincpu:internal", ROMREGION_ERASE00 )
 	//ROM_LOAD16_WORD_SWAP( "internal.rom", 0x00000, 0x40000, NO_DUMP )
 
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x200000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "arcadeclassicstetris_25q16ct_c84015.bin", 0x0000, 0x200000, CRC(a97e1bab) SHA1(400944d310d5d5fccb2c6d048d7bf0cb00da09de) )
 ROM_END
 
 
 
 ROM_START( punirune )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "25l64.ic103", 0x0000, 0x800000, CRC(0737edc0) SHA1(fce19d91a0522a75e676197fb18645b8c6a273b8) )
 ROM_END
 
 ROM_START( punij1m )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "japan_v1pcb_mint_25l6433f.ic103", 0x0000, 0x800000, CRC(76f28b5b) SHA1(be04d60c88df52951dd51eab2f5bf5f1dc2405e8) )
 ROM_END
 
 ROM_START( punij1pk ) // this might be the same software revision as punij1m with different save (or default save) data
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "japan_v1pcb_pink_25l6433f.ic103", 0x0000, 0x800000, CRC(9268c881) SHA1(10bacfa48b3d02956d804396b652829ff868d947) )
 ROM_END
 
 ROM_START( punij1pu ) // different software revision to punij1m / punij1pk but same case style
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "japan_v1pcb_purple_25l6433f.ic103", 0x0000, 0x800000, CRC(5b73bcb6) SHA1(109b6fa29693e7622c528d95d2a995d37a1cd8ca) )
 ROM_END
 
 ROM_START( punij2pk )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "japan_v2pcb_pink_gpr25l64.ic103", 0x0000, 0x800000, CRC(7ae9f009) SHA1(d762634a0442ff231837f9481a1203933c070df0) )
 ROM_END
 
 ROM_START( punifrnd )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "25oh64.ic3", 0x0000, 0x800000, CRC(622ca9b3) SHA1(4206393a4458ffcdb63352e743481865532fe8b5) )
 ROM_END
 
 ROM_START( punistar )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "xm25qh64c.ic3", 0x0000, 0x800000, CRC(72f54f23) SHA1(902955764d0b61decc057eb3afaf2960cf2134c6) )
 ROM_END
 
 ROM_START( flufflav )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x400000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "gpr25l320.u2", 0x0000, 0x400000, CRC(67bcd4cb) SHA1(5ffb140bf8e4608b5420a649aede3946923f6dac) )
 ROM_END
 
 ROM_START( pockrmsr )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "25l6433f.ic102", 0x0000, 0x800000, CRC(4b899ca1) SHA1(5e57aaa0598563c08f3b3e4f26447a3902ca51dc) )
 ROM_END
 
 ROM_START( pokgoget )
-	ROM_REGION16_BE(0x2000000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x2000000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "mx25l25645g.u1", 0x0000, 0x2000000, CRC(a76ae22f) SHA1(3fa5eeedb3fe343a7707d76710298377b22b0681) )
 ROM_END
 
 ROM_START( pokebala )
-	ROM_REGION16_BE(0x2000000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x2000000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "mx25l25645g.u4", 0x0000, 0x2000000, CRC(e35d434a) SHA1(74061831b25476ec8aa7dec5f9d64ff79b0db88e) )
 ROM_END
 
 ROM_START( pokeissh )
-	ROM_REGION16_BE(0x2000000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x2000000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "mx25l25645g.u1", 0x0000, 0x2000000, CRC(1eaf3457) SHA1(a7f16ad7abfc13c67d8e50f462882a771b6777ab) )
 ROM_END
 
 ROM_START( pokemech )
-	ROM_REGION16_BE(0x2000000, "maincpu:spidirect", ROMREGION_ERASE00)
+	ROM_REGION16_BE(0x2000000, "spi", ROMREGION_ERASE00)
 	ROM_LOAD16_WORD_SWAP( "mx25l25645g.u1", 0x0000, 0x2000000, CRC(e170dede) SHA1(4b07cfcc92e6af412ad0e5c9852b7075a15bd75c) )
 ROM_END
 
 ROM_START( smkcatch )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "gpr25l64.u2", 0x0000, 0x800000,  CRC(e2f52c4a) SHA1(f79862d27152cff8f96151c672d9762a3897a593) )
 ROM_END
 
 ROM_START( smkguras )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "gpr25l64.u3", 0x0000, 0x800000, CRC(96b61464) SHA1(59fada02e1ea983c56304b27b3f4cb1fba221cb0) )
 ROM_END
 
 ROM_START( smkgurasa ) // code is the same, some data area differs, could be different factory defaults, or user data, remove later if redundant
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "xm25qh64c.u3", 0x0000, 0x800000, CRC(2c9e82af) SHA1(8c475783eafbeaeb88d62b145758ad3487410222) )
 ROM_END
 
 ROM_START( smkgacha )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "kg25l64.u2", 0x0000, 0x800000, CRC(eb461d92) SHA1(7481e8b3f2eaa9c8d992f3d9c58c1c7e3f27b380) )
 ROM_END
 
 ROM_START( dsgnpal )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "gpr25l64.ic2", 0x0000, 0x800000, CRC(a1017ea8) SHA1(bd4b553ff71e763cd3fd726c49f5408eac3b7984) )
 ROM_END
 
 ROM_START( segapet1 )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "pink_gpr25l64.ic3", 0x0000, 0x800000, CRC(3bb709d1) SHA1(8a1b34d6cdd856685182d19b86c5cb68a006f816) )
 ROM_END
 
 ROM_START( segapet1a )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "yellow_gpr25l64.ic3", 0x0000, 0x800000, CRC(073218b8) SHA1(8501e69dc2b2cda9e4c289a14d6a16fe832e722c) )
 ROM_END
 
 ROM_START( segapet2 )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "purple_gpr25l64.ic3", 0x0000, 0x800000, CRC(e223eabf) SHA1(fa88173361af7f8cb7651bd7ccb73d7137eb6cf9) )
 ROM_END
 
 ROM_START( segapet2a )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "orange_gpr25l64.ic3", 0x0000, 0x800000, CRC(25605da6) SHA1(2733f30037551b2e5895efbb19ed953e81405141) )
 ROM_END
 
 ROM_START( segapet3 )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "pink_gpr25l64.ic3", 0x0000, 0x800000, CRC(4b60f556) SHA1(3487c7b42e1b818ac1fe3a9429320519574ca4ac) )
 ROM_END
 
 ROM_START( segapet3a )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "blue_gpr25l64.ic3", 0x0000, 0x800000, CRC(02a63c63) SHA1(54d8c1c52a30d7b2a21f69439d9ff2ef7b2a606b) )
 ROM_END
 
 ROM_START( segaptdx )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "gpr25l640.ic3", 0x0000, 0x800000, CRC(971837c5) SHA1(b0e639fbd1e2b41934a98fde8425ded84f1a8159) )
 ROM_END
 
 ROM_START( bubltea )
-	ROM_REGION16_BE(0x800000, "maincpu:spidirect", ROMREGION_ERASE00 )
+	ROM_REGION16_BE(0x800000, "spi", ROMREGION_ERASE00 )
 	ROM_LOAD16_WORD_SWAP( "gpr25l64.ic2", 0x0000, 0x800000, CRC(a6d73241) SHA1(bc67d932ffc83d91dc2d64f40bbce08c1e8b9f4e) )
 ROM_END
 
 void generalplus_gpspi_direct_game_state::init_fif()
 {
-	uint16_t* spirom16 = (uint16_t*)memregion("maincpu:spidirect")->base();
-	for (int i = 0; i < 0x800000 / 2; i++)
+	u16* spirom16 = (u16*)memregion("spi")->base();
+	for (int i = 0; i < memregion("spi")->bytes() / 2; i++)
 	{
 		spirom16[i] = bitswap<16>(spirom16[i] ^ 0xdd0d,
 			3, 1, 11, 9, 6, 14, 0, 2, 8, 7, 13, 15, 4, 5, 12, 10);
@@ -349,9 +380,9 @@ void generalplus_gpspi_direct_game_state::init_fif()
 
 	// the games upload some self-check code to 0x100 in RAM, it's unclear what it is checking, skip it for now
 	// goto mr -> nop
-	if (spirom16[0x00d8] == 0xf161) spirom16[0x00d8] = 0xf165; // fixitflx, bfpacman, bfmpac
-	if (spirom16[0x00ac] == 0xf161) spirom16[0x00ac] = 0xf165; // wiwcs, bfgalaga, bfdigdug, bfspyhnt
-	if (spirom16[0x00a2] == 0xf161) spirom16[0x00a2] = 0xf165; // bftetris
+	//if (spirom16[0x00d8] == 0xf161) spirom16[0x00d8] = 0xf165; // fixitflx, bfpacman, bfmpac
+	//if (spirom16[0x00ac] == 0xf161) spirom16[0x00ac] = 0xf165; // wiwcs, bfgalaga, bfdigdug, bfspyhnt
+	//if (spirom16[0x00a2] == 0xf161) spirom16[0x00a2] = 0xf165; // bftetris
 }
 
 } // anonymous namespace
