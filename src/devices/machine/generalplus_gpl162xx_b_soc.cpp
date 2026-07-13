6d0ce428ca38eaa679848b8e9dcdcac3c383ebcc
David Haywood <hazemamewip@hotmail.com>
2026-07-13 01:26:33 +0100

    some GPL162xx refactoring

---
+++ src/devices/machine/generalplus_gpl162xx_b_soc.cpp
@@ -0,0 +1,20 @@
+// license:BSD-3-Clause
+// copyright-holders:David Haywood
+/*****************************************************************************
+
+    GPL16218B
+    GPL16238B (this could be GPAC500B)
+    GPL16248VB
+    GPL16258VB (this could be GPAC800B)
+
+**********************************************************************/
+
+#include "emu.h"
+#include "generalplus_gpl162xx_b_soc.h"
+
+generalplus_gpl16218b_device::generalplus_gpl16218b_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock) :
+	generalplus_gpl162xx_base_device(mconfig, GPL16218B, tag, owner, clock)
+{
+}
+
+DEFINE_DEVICE_TYPE(GPL16218B, generalplus_gpl16218b_device, "gcm394", "GeneralPlus GPL16218B System-on-a-Chip")
---
+++ src/devices/machine/generalplus_gpl162xx_b_soc.h
@@ -0,0 +1,32 @@
+// license:BSD-3-Clause
+// copyright-holders:David Haywood
+/*****************************************************************************
+
+  SunPlus "GCM394" (based on die pictures)
+
+**********************************************************************/
+
+#ifndef MAME_MACHINE_GENERALPLUS_GPL162XX_B_SOC_H
+#define MAME_MACHINE_GENERALPLUS_GPL162XX_B_SOC_H
+
+#pragma once
+
+#include "generalplus_gpl162xx_soc.h"
+
+class generalplus_gpl16218b_device : public generalplus_gpl162xx_base_device
+{
+public:
+	template <typename T>
+	generalplus_gpl16218b_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock, T &&screen_tag) :
+		generalplus_gpl16218b_device(mconfig, tag, owner, clock)
+	{
+		m_screen.set_tag(std::forward<T>(screen_tag));
+	}
+
+	generalplus_gpl16218b_device(const machine_config &mconfig, const char *tag, device_t *owner, u32 clock);
+};
+
+DECLARE_DEVICE_TYPE(GPL16218B, generalplus_gpl16218b_device)
+
+
+#endif // MAME_MACHINE_GENERALPLUS_GPL162XX_B_SOC_H
